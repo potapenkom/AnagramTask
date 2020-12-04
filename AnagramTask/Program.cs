f5e741387f24f41c36513d30a34846322697cdd1
Misha Potapenko <potapenko05121985@gmail.com>
2020-12-05 00:33:22 +0200

    added solution

--- AnagramTask/Program.cs
+++ AnagramTask/Program.cs
@@ -6,7 +6,15 @@ namespace AnagramTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Input the string : ");
+            string input = Console.ReadLine();
+            if (String.IsNullOrEmpty(input))
+            {
+                throw new NullReferenceException("You entered an empty string");
+            }
+            UerInput uerInput = new UerInput(input);
+            Console.WriteLine($"Inverted line: {uerInput.Reverse()}");
+            Console.ReadKey();
         }
     }
 }
---
+++ AnagramTask/UerInput.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AnagramTask
+{
+    public class UerInput
+    {
+        private string _input;
+        public UerInput(string input)
+        {
+            _input = input;
+        }
+
+        public bool IsLettersOrDigits(string s)
+        {
+            foreach (var c in s)
+            {
+                if (!char.IsLetterOrDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public string Reverse(char[] s)
+        {
+            Array.Reverse(s);
+            return new string(s);
+        }
+        public string Reverse()
+        {
+            var parts = Regex.Split(_input, @"([^a-zA-Z])");
+
+            var results = new List<string>();
+            foreach (var x in parts)
+            {
+                results.Add(IsLettersOrDigits(x)
+                           ? Reverse(x.ToCharArray())
+                           : x);
+            }
+            return string.Concat(results);
+        }
+    }
+}
+
